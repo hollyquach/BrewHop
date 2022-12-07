3a0db17ae8840184ac85bb5b66a93e0379d93be7
Jeff Pettit <jwpettit@me.com>
2022-12-07 02:01:19 +0000

    Pass userName userID Hooks

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useLocalStorage } from 'usehooks-ts'
 import { Route, Routes } from 'react-router-dom'
 import './App.scss';
 import Featured from './Router/Featured/Featured.jsx'
@@ -13,49 +14,53 @@ export default function App() {
   const [searchCity, setSearchCity] = useState('') // search param - user input in search form -> results
   const [searchState, setSearchState] = useState('') // search param - user input in search form -> results
   const [loginStatus, setLoginStatus] = useState(false)
-  const [userID, setUserID] = useState('') // int of userID
-  const [userName, setUserName] = useState('')
+  // const [userID, setUserID] = useState(JSON.parse(window.sessionStorage.getItem("userID")) || null) // int of userID
+  // const [userName, setUserName] = useState(JSON.parse(window.sessionStorage.getItem("userName")) || '')
+  const [userID, setUserID] = useLocalStorage("userID", null) // int of userID
+  const [userName, setUserName] = useLocalStorage("userName", '')
   const [userFavorites, setUserFavorites] = useState([]) // list of yelp_ids -> user's favorited breweries
   const [breweryYelpID, setBreweryYelpID] = useState('') // string of a single yelp ID
   const [showLoginForm, setShowLoginForm] = useState(false)
   const [showSignupForm, setShowSignupForm] = useState(false)
   const [token] = useToken();
 
-  useEffect(() => { setLoginStatus(token ? true : false) }, [token]);
+  useEffect(() => {
+    setLoginStatus(token ? true : false);
+
+  }, [token]);
 
 
   // [] function to update favorites (add or delete)
 
-    const getUserFavorites = async() => {
-        // only runs if user is logged in
-        if (loginStatus) {
-            let url = `${process.env.REACT_APP_FAVORITES_SERVICE_API_HOST}/favorites/${userID}`
-            let config = {
-                method: "GET",
-                headers: {
-                  Authorization: `Bearer ${token}`,
-                },
-            }
-            console.info(`🚦⭐️⭐️ QUERYING FAVORITES SERVICE | ${url}`)
+  const getUserFavorites = async () => {
+    // only runs if user is logged in
+    if (loginStatus) {
+      let url = `${process.env.REACT_APP_FAVORITES_SERVICE_API_HOST}/favorites/${userID}`
+      let config = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+      console.info(`🚦⭐️⭐️ QUERYING FAVORITES SERVICE | ${url}`)
 
-            const response = await fetch (url, config);
-            if (response.ok) {
-                let data = await response.json();
-                setUserFavorites(data)
-            } else {
-                console.error(`🛑🛑 ERROR getting user favorites |`, response);
-            } 
-        } else {
-            console.debug(`🚦🚦 getUserFavorites | NOT LOGGED IN`);
-        }
+      const response = await fetch(url, config);
+      if (response.ok) {
+        let data = await response.json();
+        setUserFavorites(data)
+      } else {
+        console.error(`🛑🛑 ERROR getting user favorites |`, response);
+      }
+    } else {
+      console.debug(`🚦🚦 getUserFavorites | NOT LOGGED IN`);
     }
+  }
 
-    useEffect(() => {
-        setUserID(1);
-        getUserFavorites()
-    }, [breweryYelpID]);
+  useEffect(() => {
+    getUserFavorites();
+  }, [userID]);
 
-    
+
   // !! from original fork & left for ref only -> remove before deployment
   /*
     const [launch_info, setLaunchInfo] = useState([]);
@@ -82,33 +87,36 @@ export default function App() {
   */
 
   // [] pass props into components as required
-    return (
-        <div>
-            <Routes>
-                <Route path="/" element={<Layout
-                    setSearchCity={setSearchCity}
-                    setSearchState={setSearchState}
-                    loginStatus={loginStatus}
-                    setLoginStatus={setLoginStatus}
-                    userName={userName}
-                    showSignupForm={showSignupForm}
-                    setShowSignupForm={setShowSignupForm}
-                    showLoginForm={showLoginForm}
-                    setShowLoginForm={setShowLoginForm}
-                />} >
-                <Route index element={<Featured setID={setBreweryYelpID} />} />
-                <Route path="search/" element={
-                    <Results 
-                    searchCity={searchCity} searchState={searchState}
-                    loginStatus={loginStatus}
-                    userFavorites={userFavorites} setUserFavorites={setUserFavorites}
-                    breweryYelpID={breweryYelpID} setBreweryYelpID={setBreweryYelpID}/>
-                } />
-                <Route path="favorites/" element={<Favorites />} />
-                <Route path="brewery/" element={<Brewery yelpID={breweryYelpID} />} />
-                <Route path="*" element={<Invalid />} />
-                </Route>
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout
+          setSearchCity={setSearchCity}
+          setSearchState={setSearchState}
+          loginStatus={loginStatus}
+          setLoginStatus={setLoginStatus}
+          showSignupForm={showSignupForm}
+          setShowSignupForm={setShowSignupForm}
+          showLoginForm={showLoginForm}
+          setShowLoginForm={setShowLoginForm}
+          userName={userName}
+          setUserName={setUserName}
+          userID={userID}
+          setUserID={setUserID}
+        />} >
+          <Route index element={<Featured setID={setBreweryYelpID} />} />
+          <Route path="search/" element={
+            <Results
+              searchCity={searchCity} searchState={searchState}
+              loginStatus={loginStatus}
+              userFavorites={userFavorites} setUserFavorites={setUserFavorites}
+              breweryYelpID={breweryYelpID} setBreweryYelpID={setBreweryYelpID} />
+          } />
+          <Route path="favorites/" element={<Favorites />} />
+          <Route path="brewery/" element={<Brewery yelpID={breweryYelpID} />} />
+          <Route path="*" element={<Invalid />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
