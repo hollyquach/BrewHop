fd8f15f9ad9670ae7d1ebf79a2c6938e280e233d
Yaning Wang <xyaningwangx@gmail.com>
2022-12-09 22:56:48 +0000

    Unit Test Complete and Connection pool resolved for accounts_service

--- accounts_service/Test/test_accounts.py
+++ accounts_service/Test/test_accounts.py
@@ -2,6 +2,7 @@ from main import app
 from fastapi.testclient import TestClient
 from queries.accounts import AccountsRepository
 
+
 client = TestClient(app)
 
 test_users = {
@@ -13,14 +14,22 @@ test_users = {
 
 
 class EmptyAccountsRepository:
-    def get_one_user(self, user_id: int):
+    def get_one(self, user_id: int):
         return test_users
 
 
 def test_get_one_user_by_id():
-    app.dependency_overrides[AccountsRepository] = EmptyAccountsRepository
-    response = client.get("/accounts/18")
-    app.dependency_overrides = {}
 
+    # Arrange
+    app.dependency_overrides[AccountsRepository] = EmptyAccountsRepository
+
+    # Act
+    response = client.get("/accounts/18")
+    # app.dependency_overrides = {}
+
+    # Assert
     assert response.status_code == 200
     assert response.json() == test_users
+
+    # clean up
+    app.dependency_overrides = {}
