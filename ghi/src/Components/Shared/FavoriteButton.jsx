d53e3b56e51fb46a182862ecc2d95eee31e9250b
Holly Quach <hollyquach@gmail.com>
2022-12-09 19:22:51 -0800

    Update 79 feature branch - merging in main

--- ghi/src/Components/Shared/FavoriteButton.jsx
+++ ghi/src/Components/Shared/FavoriteButton.jsx
@@ -1,5 +1,5 @@
 import Button from 'react-bootstrap/Button';
-import { useToken } from '../useToken'
+import { useAuthContext } from '../useToken'
 
 const FavoriteButton = ({ breweryYelpID, userFavorites, userID }) => {
     const { token } = useAuthContext();
--- ghi/src/index.js
+++ ghi/src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthProvider } from './Components/useToken.js'
-import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider, useToken } from "./Components/useToken";
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const domain = /https:\/\/[^/]+/;
